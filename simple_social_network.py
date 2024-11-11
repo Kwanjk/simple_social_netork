0b67d4c2eecefcf7ba518a1548dcc4629c25d1fb
Josh Kwan <kwanjosh25@gmail.com>
2024-11-11 14:22:08 -0500

    added simple_social_network.py

---
+++ simple_social_network.py
@@ -0,0 +1,13 @@
+""" A simple social network for tracking connections between people. """
+
+
+class person: 
+    """ A person in the social netwwork.
+    
+    Attributes: 
+        name(str): The person's name 
+        connections(set of person): other people in the social network who 
+            know this person
+        
+    """
+    
